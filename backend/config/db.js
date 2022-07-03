14b8d9bbb3ec729c762036f8630bf98193862ec1
Your Name <kirtanbhavsar.2491@gmail.com>
2022-07-03 22:45:28 +0530

    Changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -23,3 +23,5 @@ export default connectDB;
 /*
 [{"product":"62b99abe6141fc35fd065836","name":"The One Thing ~ Gary Keller & Jay Papasan","image":"/images/thing2.jpg","price":"299","countInStock":7,"qty":1},{"product":"62b99abe6141fc35fd065833","name":"Atomic Habits ~ James Clear","image":"/images/habits.jpg","price":"189","countInStock":4,"qty":2},{"product":"62b99abe6141fc35fd065832","name":"The Subtle Art of Not Giving Fu*k ~ Mark Manson","image":"/images/art2.jpg","price":"149","countInStock":2,"qty":2}]
 */
+
+//Check for git hub
