865efe428cb3d78d129f49289840c9e73b755f8b
Your Name <kirtanbhavsar.2491@gmail.com>
2022-07-08 00:29:41 +0530

    Final

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js"; //for 12.7
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -22,12 +24,21 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+// for 12.7
+app.use("/api/uploads", uploadRoutes);
+//ends 12.7
 // for 10.6
 app.get("/api/config/paypal", (req, res) => {
   res.send(
     "AfzdgJ1lLi3m54PmkwIcTpjOsUxm-qBa9Ng2pOXZ3h4KdPDUrNK9MP6CfbX9yt0AMSAQuKcJrB51_K1x"
   );
 });
+
+//for 12.7
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+//ends 12.7
+
 // ends 10.6
 app.use(notFound);
 
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
@@ -21,6 +22,7 @@ const ProductEditScreen = ({ match, history }) => {
   const [category, setCategory] = useState("");
   const [countInStock, setCountInStock] = useState(0);
   const [description, setDescription] = useState("");
+  const [uploading, setUploading] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -71,6 +73,29 @@ const ProductEditScreen = ({ match, history }) => {
   //   e.preventDefault();
   //   //UPDATE PRODUCT
   // };
+
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+    setUploading(true);
+
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
+
+      const { data } = await axios.post("/api/upload", formData, config);
+
+      setImage(data);
+      setUploading(false);
+    } catch (error) {
+      console.error(error);
+      setUploading(false);
+    }
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -132,8 +157,8 @@ const ProductEditScreen = ({ match, history }) => {
                 onChange={(e) => setImage(e.target.value)}
               ></Form.Control>
               {/* <Form.File
-                id='image-file'
-                label='Choose File'
+                id="image-file"
+                label="Choose File"
                 custom
                 onChange={uploadFileHandler}
               ></Form.File> */}
@@ -190,5 +215,4 @@ const ProductEditScreen = ({ match, history }) => {
     </>
   );
 };
-
 export default ProductEditScreen;
