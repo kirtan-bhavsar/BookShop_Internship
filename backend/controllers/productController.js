62b8944104448738d8a58a5f7324f374f57cef89
Your Name <kirtanbhavsar.2491@gmail.com>
2022-07-07 19:22:36 +0530

    12.3

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -38,5 +38,61 @@ const deleteProduct = asyncHandler(async (req, res) => {
 });
 //ends 12.2
 
-export { getProductById, getProducts, deleteProduct };
-//if 12.2 fails then delete the deleteProduct
+//starts 12.3 for create a product
+// @description Create a product
+// @route POST /api/products
+// @access Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample Name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample Brand",
+    category: "Sample Category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+//ends 12.3 for update a product
+
+//starts 12.3 for update a product
+// @description Update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updateProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+//ends 12.3 for update a product
+
+export {
+  getProductById,
+  getProducts,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
+//if 12.3 fails then delete the createProduct,updateProduct
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,10 +4,16 @@ import {
   getProductById,
   getProducts,
   deleteProduct, //for 12.2
+  createProduct, //for 12.3
+  updateProduct, //for 12.3
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getProducts);
-router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct); //if 12.2 fails then delete the       .delete and deleteProduct from imported controllers
-
+router.route("/").get(getProducts).post(protect, admin, createProduct); //if 12.3 fails then delete .post
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct); //if 12.2 fails then delete the       .delete and deleteProduct from imported controllers
+// if 12.3 fails then delete .put
 export default router;
